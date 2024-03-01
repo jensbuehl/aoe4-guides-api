109a6704d49a76f21e304da9b88911697fc84285
Jens Bühl <jbuehl@itemis.com>
2024-03-01 08:24:48 +0100

    Use application default credentials to make it work on cloud run

--- firebase.js
+++ firebase.js
@@ -1,10 +1,8 @@
 import admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
 
-import serviceAccount from "./aoe4-guides-dev-firebase-adminsdk-7l6v8-0e88eb6085.json" assert { type: "json" };
-
 const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.applicationDefault(),
 });
 const db = getFirestore(app);
 
