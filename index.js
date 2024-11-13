e320133f8796868778214552a7ccb64305e6631f
Jens Bühl <jbuehl@itemis.com>
2024-11-13 11:27:05 +0100

    Configure and enable CORS (since it's a public API)

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import swagger from './swagger.js';
 import {getAll, getById, getFavorites} from './builds.js';
 
 const app = express();
-app.use(express.json());
+app.use(express.json(), cors());
+
+const corsOptions = {
+  "origin": "*",
+  "optionsSuccessStatus": 200
+};
+
+app.use(cors(corsOptions));
 
 /**
  * @openapi
