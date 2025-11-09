86f8b6928c0c48932f0fd65850ceb7124a93d9c9
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-11-09 05:02:59 +0100

    feat: add overlay return types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import { TsoaResponse } from "tsoa";
 
 export * from "./civilization";
+export * from "./overlay";
 export * from "./queries";
 
 export type NotFoundResponse = TsoaResponse<404, { reason: string }>;
