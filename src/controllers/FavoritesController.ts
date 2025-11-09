3906f1c79e683da21f4ed8e40598dd88bc16a054
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-11-09 03:38:14 +0100

    feat: add controllers for api routes

---
+++ src/controllers/FavoritesController.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Path, Queries, Res, Route, SuccessResponse, Tags } from "tsoa";
+import { BuildOrder, BuildOrders } from "../models";
+import { BuildQuery, NotFoundResponse } from "../types";
+import db from "../db";
+
+
+@Route("favorites")
+@Tags("Favorites")
+export class FavoritesController extends Controller {
+    /**
+     * Get user's favorite builds
+     * @param userId The user's identifier
+     */
+    @Get("{userId}")
+    @SuccessResponse("200", "OK")
+    public async getUserFavorites(@Path() userId: string, @Queries() q: BuildQuery, @Res() notFoundResponse: NotFoundResponse): Promise<BuildOrders> {
+        const snapshot = db.collection("favorites").doc(userId);
+        const user = await snapshot.get();
+
+        if (!user.exists)
+            return notFoundResponse(404, { reason: "" });
+
+        const favorites = user.data()?.favorites as string[] | undefined;
+
+        if (!favorites || favorites.length === 0)
+            return [];
+
+        let query = db
+            .collection("builds")
+            .where("id", "in", user.data()?.favorites)
+            .limit(10);
+
+        if (q.civ)
+            query = query.where("civ", "in", [q.civ]);
+
+        if (q.orderBy)
+            query = query.orderBy(q.orderBy, "desc");
+
+        const builds = await query.get();
+
+        return builds.docs.map((doc) => doc.data() as BuildOrder);
+    }
+}
